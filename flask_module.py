4cc8e97f958d165de1294beaa9ddacac4f55d1ed
Eric Rojas <rojase10@uci.edu>
2024-05-25 19:11:06 -0700

    Flask/Index updated
    Added survey function in Flask. Redirects to the actual survey when the link is entered. Index was changed to appropriately route HTML pages.

--- flask_module.py
+++ flask_module.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,8 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/survey')
+def survey():
+    return render_template('survey.html')
 
-@app.route('/', methods = ['POST'])
+@app.route('/submit', methods = ['POST'])
 def submit_form():
     data = request.form
     pointOfInterest = data.get('pointOfInterest')
@@ -17,7 +20,7 @@ def submit_form():
 
     print(pointOfInterest)
     print(preDeterminedQuestions)
-    return pointOfInterest,preDeterminedQuestions, 200
+    return jsonify(pointOfInterest,preDeterminedQuestions), 200
 
 if __name__ == '__main__':
     app.run(port=8000)
