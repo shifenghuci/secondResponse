3a66ed4480c35637f51d3a39e19937326d4906ab
Eric Rojas <rojase10@uci.edu>
2024-05-25 18:49:56 -0700

    Index's script was updated. flask_module too

--- flask_module.py
+++ flask_module.py
@@ -1,9 +1,23 @@
-from flask import Flask,request, render_template
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def index()
+def index():
     return render_template('index.html')
 
-@app.route('/submit_form', methods = ['POST'])
-def submit_form
+@app.route('/', methods = ['POST'])
+def submit_form():
+    data = request.form
+    pointOfInterest = data.get('pointOfInterest')
+    preDeterminedQuestions = data.get('preDeterminedQuestions')
+
+
+
+    print(pointOfInterest)
+    print(preDeterminedQuestions)
+    return pointOfInterest,preDeterminedQuestions, 200
+
+if __name__ == '__main__':
+    app.run(port=8000)
